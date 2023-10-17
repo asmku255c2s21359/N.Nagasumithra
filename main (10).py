45c97137538aa775caccc0c14784613957ca9872
asmku255c2s21359 <147711888+asmku255c2s21359@users.noreply.github.com>
2023-10-17 20:37:25 +0530

    Add files via upload

---
+++ main (10).py
@@ -0,0 +1,19 @@
+...
+1!1*1
+2!=2*1!->2*1
+3!=3*2*1!->3*2*1
+.
+.
+.
+.
+10!=10*9!->10*9*8*...*1
+formula_n*(n-1)1
+....
+def fact_rec(n):
+  if(n==0 or n==1):
+    return 1
+  else:
+    return n*fact_rec(n-1)
+    number=int(input("enter a number:"))
+    res=fact_rec(number)
+    print("the factorial of {} is {}" format number,res)))
